3fece58f1469a6edbaa72e5d5a7c4c0892a932cc
neko3cs <minaton.0722.net@gmail.com>
2021-02-24 00:53:56 +0900

    【WIP】DailyWorkServiceのユニットテスト修正（delete以外）

--- src/client/src/app/services/daily-work.service.spec.ts
+++ src/client/src/app/services/daily-work.service.spec.ts
@@ -1,20 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-
+import { of } from 'rxjs';
 import { DailyWorkService } from './daily-work.service';
 import { DailyWork } from '../data/daily-work';
 
 describe('DailyWorkService', () => {
+  let httpClientSpy: { get: jasmine.Spy, delete: jasmine.Spy };
   let service: DailyWorkService;
-  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
       providers: [DailyWorkService]
-    });
-    service = TestBed.inject(DailyWorkService);
-    httpMock = TestBed.inject(HttpTestingController);
+    }).compileComponents();
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'delete']);
+    service = new DailyWorkService(httpClientSpy as any);
   });
 
   it('should be created', () => {
@@ -22,24 +20,37 @@ describe('DailyWorkService', () => {
   });
 
   it('should be get all daily works.', () => {
+    const expected = [
+      new DailyWork(1, "就寝", new Date("2000-01-01 13:00:00.000 +0900Z"), new Date("2000-01-01 20:30:00.000 +0900Z")),
+      new DailyWork(2, "起床", new Date("2000-01-01 20:30:00.000 +0900Z"), new Date("2000-01-01 21:00:00.000 +0900Z")),
+      new DailyWork(3, "朝活", new Date("2000-01-01 21:00:00.000 +0900Z"), new Date("2000-01-01 0:00:00.000 +0900Z"))
+    ];
+
+    httpClientSpy.get.and.returnValue(of(expected));
+
     service.getDailyWorks().subscribe(
-      dailyWorks => expect(dailyWorks.length).toBe(10)
+      dailyWorks => expect(expected).toEqual(dailyWorks)
     );
+    expect(httpClientSpy.get.calls.count()).toBe(1);
   });
 
-  it('should be deleted', () => {
-    let beforeDeletedWorks: DailyWork[] = [];
-    service.getDailyWorks().subscribe(
-      works => beforeDeletedWorks = works
-    );
-    let deleteWork = beforeDeletedWorks.slice(0, 1)[0];
+  it('should be delete the daily work.', () => {
+    const deleted = new DailyWork(3, "朝活", new Date("2000-01-01 21:00:00.000 +0900Z"), new Date("2000-01-01 0:00:00.000 +0900Z"));
+    const after = [
+      new DailyWork(1, "就寝", new Date("2000-01-01 13:00:00.000 +0900Z"), new Date("2000-01-01 20:30:00.000 +0900Z")),
+      new DailyWork(2, "起床", new Date("2000-01-01 20:30:00.000 +0900Z"), new Date("2000-01-01 21:00:00.000 +0900Z"))
+    ];
+    const before = after.concat(deleted);
+    httpClientSpy.get.and.returnValue(of(before));
+    httpClientSpy.delete.and.returnValue(of(after));
 
-    let afterDeletedWorks: DailyWork[] = [];
-    service.deleteDailyWork(deleteWork);
-    service.getDailyWorks().subscribe(
-      works => afterDeletedWorks = works
+    service.getDailyWorks().subscribe();
+    expect(httpClientSpy.get.calls.count()).toBe(1);
+
+    // FIXME: 戻り値がおかしい（HttpClient#deleteの仕様を確認して、実装を直すこと）
+    service.deleteDailyWork(deleted).subscribe(
+      dailyWork => expect(deleted).toEqual(dailyWork)
     );
-    expect(beforeDeletedWorks.length - 1).toBe(afterDeletedWorks.length);
-    expect(afterDeletedWorks).not.toContain(deleteWork);
+    expect(httpClientSpy.delete.calls.count()).toBe(1);
   });
 });

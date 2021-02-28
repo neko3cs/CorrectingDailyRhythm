1a117c04e02210a33d73a7e7fe342a16c41f4607
neko3cs <minaton.0722.net@gmail.com>
2021-03-01 01:02:07 +0900

    【WIP】DailyWorkService#deleteDailyWork のテスト

--- src/client/src/app/services/daily-work.service.spec.ts
+++ src/client/src/app/services/daily-work.service.spec.ts
@@ -37,28 +37,25 @@ describe('DailyWorkService', () => {
 
         const testRequest = httpMock.expectOne({ url: `${dailyWorksUrlBase}/dailyWork` });
         expect(testRequest.request.method).toEqual("GET");
-
         testRequest.flush({ body: responseMock });
 
         httpMock.verify();
       })));
 
-  // it('should be delete the daily work.', () => {
-  //   const deleted = new DailyWork(3, "朝活", new Date("2000-01-01 21:00:00.000 +0900Z"), new Date("2000-01-01 0:00:00.000 +0900Z"));
-  //   const afterExpected = [
-  //     new DailyWork(1, "就寝", new Date("2000-01-01 13:00:00.000 +0900Z"), new Date("2000-01-01 20:30:00.000 +0900Z")),
-  //     new DailyWork(2, "起床", new Date("2000-01-01 20:30:00.000 +0900Z"), new Date("2000-01-01 21:00:00.000 +0900Z"))
-  //   ];
-  //   const beforeExpected = afterExpected.concat(deleted);
-  //   httpClientSpy.get.and.returnValue(of(beforeExpected));
-  //   httpClientSpy.delete.and.returnValue(of(deleted));
+  it('should be delete the daily work.',
+    fakeAsync(() =>
+      inject([DailyWorkService, HttpTestingController], (service: DailyWorkService, httpMock: HttpTestingController) => {
+        const deleted = new DailyWork(3, "朝活", new Date("2000-01-01 21:00:00.000 +0900Z"), new Date("2000-01-01 0:00:00.000 +0900Z"));
 
-  //   service.getDailyWorks().subscribe(
-  //     dailyWorks => expect(dailyWorks).toEqual(beforeExpected)
-  //   );
+        service.deleteDailyWork(deleted).subscribe(
+          dailyWork => expect(dailyWork).toEqual(deleted)
+        );
 
-  //   service.deleteDailyWork(deleted);
-  //   expect(httpClientSpy.delete.calls.count()).toBe(1);
-  // });
+        const testRequest = httpMock.expectOne({ method: "DELETE", url: `${dailyWorksUrlBase}/dailyWork/${deleted.id}` });
+        expect(testRequest.request.method).toEqual("DELETE");
+        testRequest.flush({ body: null });  // FIXME: null返してもテスト通る
+
+        httpMock.verify();
+      })));
 
 });
